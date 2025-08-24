f475f6ceb518c37cd3287b322f1b6738ff5671b0
Yehia <yehia.noureldin.28@gmail.com>
2025-08-24 15:46:36 +0300

    Add project files.

---
+++ ExaminationSystem/Exam/Exam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationSystem.Exam
+{
+    internal abstract class Exam
+    {
+        public int TimeOfExam { get; set; }
+        public int NumberOfQuestion { get; set; }
+
+        public abstract void ShowExam();
+    }
+}
